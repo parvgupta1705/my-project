d093bd2a3194b5cff50a7dbc6e26ee0c4dcb1e90
parvgupta1705 <23bai70205@cuchd.in>
2025-08-20 20:25:15 +0530

    Delete my-project directory

--- my-project/Experiment-4/script.js
+++
@@ -1,6 +0,0 @@
-let textInput = document.getElementById("textInput");
-let charCount = document.getElementById("charCount");
-
-textInput.addEventListener("input", function() {
-  charCount.textContent = textInput.value.length;
-});
